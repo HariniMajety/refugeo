e26fc7accdfde88a647937f4063a996d5cda7bf9
Afrin Momin <afrinmomin06@gmail.com>
2021-07-02 10:02:22 -0500

    random changes

--- refugeo/ViewController.swift
+++ refugeo/ViewController.swift
@@ -9,7 +9,7 @@ import UIKit
 
 class ViewController: UIViewController {
     
-    var quotes = ["'The service you do for others is the rent you pay for your room here on Earth' - Muhammad Ali", "'We rise by lifting others.' - Robert Ingersoll", "'Nobody cares how much you know until they know how much you care.' - Theodore Roosevelt", "'Always be a little kinder than necessary' - James M. Barrie", "'You don’t have a garden just rof yourself. You have to share.' - Augusta Carter"]
+    var quotes = ["'The service you do for others is the rent you pay for your room here on Earth' - Muhammad Ali", "'We rise by lifting others.' - Robert Ingersoll", "'Nobody cares how much you know until they know how much you care.' - Theodore Roosevelt", "'Always be a little kinder than necessary' - James M. Barrie", "'You don’t have a garden just for yourself. You have to share.' - Augusta Carter"]
     
     @IBOutlet weak var quoteLabel: UILabel!
     
